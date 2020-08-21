b6c4655e6ff252c0537b7d3aab5bbb77428b1bb7
itsubaki <itsubaki@users.noreply.github.com>
2020-08-21 21:24:00 +0900

    Add 17_hard_test.go

---
+++ 17_hard_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestAddWithoutPlus(t *testing.T) {
+	Add := func(a, b int) int {
+		for {
+			if b == 0 {
+				break
+			}
+
+			sum := a ^ b
+			carry := (a & b) << 1
+
+			a, b = sum, carry
+		}
+
+		return a
+	}
+
+	cases := []struct {
+		a, b, c int
+	}{
+		{10, 5, 15},
+		{2, 15, 17},
+	}
+
+	for _, c := range cases {
+		result := Add(c.a, c.b)
+		if result == c.c {
+			continue
+		}
+
+		t.Errorf("expected=%v, actual=%v", c.c, result)
+	}
+}
